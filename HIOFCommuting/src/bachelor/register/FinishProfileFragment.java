5cc7f487bd05cb6c6610b692d240f6c76ba1a36a
Patrick <patricmj@hotmail.com>
2014-03-27 16:01:47 +0100

    Fjerna noen comments -> test push

--- HIOFCommuting/src/bachelor/register/FinishProfileFragment.java
+++ HIOFCommuting/src/bachelor/register/FinishProfileFragment.java
@@ -23,28 +23,4 @@ public class FinishProfileFragment extends Fragment {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_finish_profile, container, false);
 	}
-	
-	/*@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		List<String> institutionList = new ArrayList<String>();
-		institutionList.add("Hiÿ");
-		institutionList.add("HiB");
-		institutionList.add("HiT");
-		addItemsOnSpinners(institutionList); //error inflating class fragment
-		super.onActivityCreated(savedInstanceState);
-	}
-
-	public void addItemsOnSpinners(List list) {
-		institutionSpinner = (Spinner) getView().findViewById(R.id.institutionSpinner);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		institutionSpinner.setAdapter(adapter);
-	}
-	
-	public void finishButtonClicked(View view) {
-		Toast.makeText(getActivity(), 
-				"OnClickListener : " + 
-				"\n Spinner1 : " + String.valueOf(institutionSpinner.getSelectedItem()), Toast.LENGTH_SHORT);
-	}
-	*/
 }
